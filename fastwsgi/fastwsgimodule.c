28a2c18b7e718790dec17bc3544460633b81248e
remittor <remittor@gmail.com>
2026-02-26 14:38:10 +0300

    fastwsgimodule: Cleanup code

--- fastwsgi/fastwsgimodule.c
+++ fastwsgi/fastwsgimodule.c
@@ -3,19 +3,23 @@
 
 static PyMethodDef FastPySgiFunctions[] = {
     { "init_server", init_server, METH_O, "" },
-    { "change_setting", change_setting, METH_VARARGS, "" },
-    { "run_server", run_server, METH_O, "" },
-    { "run_nowait", run_nowait, METH_O, "" },
-    { "close_server", close_server, METH_O, "" },
-    { NULL, NULL, 0, NULL}
+    { "change_setting", change_setting, METH_VARARGS, NULL },
+    { "run_server", run_server, METH_O, NULL },
+    { "run_nowait", run_nowait, METH_O, NULL },
+    { "close_server", close_server, METH_O, NULL },
+    { NULL }
 };
 
 static struct PyModuleDef module = {
-    PyModuleDef_HEAD_INIT,
-    "fastpysgi",
-    "fastpysgi Python module",
-    -1,
-    FastPySgiFunctions,
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "fastpysgi",
+    .m_doc  = "FastPySGI Python module",
+    .m_size = -1,
+    .m_methods = FastPySgiFunctions,
+    .m_slots = NULL,
+    .m_traverse = NULL,
+    .m_clear = NULL,
+    .m_free = NULL,
 };
 
 PyMODINIT_FUNC PyInit__fastpysgi(void)
